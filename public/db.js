465ec22a60ee8d605692ca388241583fe5bb3ab4
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 16:16:40 -0500

    make sure app is online

--- public/db.js
+++ public/db.js
@@ -7,8 +7,13 @@ const indexedDB =
 
 let db;
 const request = indexedDB.open("budget", 1);
-
 request.onupgradeneeded = ({ target }) => {
   let db = target.result;
   db.createObjectStore("pending", { autoIncrement: true });
+};
+request.onsuccess = ({ target }) => {
+  db = target.result;
+  if (navigator.onLine) {
+    checkDatabase();
+  }
 };
