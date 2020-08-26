1ef31b2e7b6f3cb0ca4d2a9114d374f6a2be493e
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 16:15:17 -0500

    return cached home page requests for html page

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,21 +17,36 @@ self.addEventListener("install", function (event) {
     })
   );
 });
-self.addEventListener("fetch", function(event) {
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
           return fetch(event.request)
-            .then(response => {
+            .then((response) => {
               if (response.status === 200) {
                 cache.put(event.request.url, response.clone());
               }
               return response;
             })
-            .catch(err => {
+            .catch((err) => {
               return cache.match(event.request);
             });
-        }).catch(err => console.log(err))
-      );
-      return;
-    }
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
