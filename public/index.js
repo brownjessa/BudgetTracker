c6bc6909ac3aad408b88dbe05a5aea1e3eb8e822
Jessamine Brown <brownjessa11@gmail.com>
2020-08-26 15:57:45 -0500

    reduce transaction amounts to single total value

--- public/index.js
+++ public/index.js
@@ -17,3 +17,11 @@ fetch("/api/transaction")
     populateTable();
     populateChart();
   });
+function populateTotal() {
+  // reduce transaction amounts to a single total value
+  let total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
+  let totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
