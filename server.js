7aad1a353958a26992ca93a9a596079ae4f10980
Jessamine Brown <brownjessa11@gmail.com>
2020-08-27 09:53:33 -0500

    updating port and sql fix

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3006;
+var PORT = process.env.PORT || 8080;
 
+var db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
@@ -17,7 +18,9 @@ app.use(express.static("public"));
 
 // routes
 app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+// sync sequelize model
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 });
